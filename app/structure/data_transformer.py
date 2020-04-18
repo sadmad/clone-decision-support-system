cf06c7aae6148155f3e8044f3d2e5c67a37bda17
Awais <ovaiz.mushtaq@gmail.com>
2020-04-18 05:11:04 +0200

    converting data after parsing into csv

--- app/structure/data_transformer.py
+++ app/structure/data_transformer.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from app import app
 import os
 import json
@@ -5,6 +7,51 @@ import requests
 import hashlib
 import base64
 from requests_hawk import HawkAuth
+
+
+def egeos_authentication_api():
+
+    base_url = app.config['EGEOS']['base_url']
+    r_login_request = requests.post(base_url + '/auth/login_request',
+                                    data={'username': app.config['EGEOS']['user_name']})
+    data = r_login_request.json()
+    if "code" not in data or data["code"] != 401005:
+        print(r_login_request.json())
+        challenge = data['challenge']
+        login_id = data['login_id']
+        salt = data['salt']
+        password = app.config['EGEOS']['password']
+
+        salt = base64.b64decode(salt)
+        challenge = base64.b64decode(challenge)
+        concated = salt + password.encode('utf-8')
+        currentHash = concated
+        i = 0
+        while i < 100000:
+            hash_object = hashlib.sha256()
+            hash_object.update(currentHash)
+            currentHash = hash_object.digest()
+            i += 1
+        result = salt + currentHash
+        digest1 = "digest1:" + str(base64.b64encode(result), 'utf-8')
+
+        currentHash = salt + challenge + currentHash
+        i = 0
+        while i < 5:
+            hash_object2 = hashlib.sha256()
+            hash_object2.update(currentHash)
+            currentHash = hash_object2.digest()
+            i += 1
+
+        challenge_response = str(base64.b64encode(currentHash), 'utf-8')
+        headers = {"Accept-Language": "en-US,en;q=0.9,ur;q=0.8", "language": "eng"}
+        challenge = str(base64.b64encode(challenge), 'utf-8')
+        r_login = requests.post('https://www.amucad.org/auth/login',
+                                data={'login_id': login_id, 'challenge': challenge,
+                                      'challenge_response': challenge_response}, headers=headers)
+        return r_login.json()
+    else:
+        return data
 
 
 class DataTransformer:
@@ -16,15 +63,14 @@ class DataTransformer:
         data = {}
         for key in self.__data:
             data[key] = {}
-
+            data[key]['id'] = key
             self.extract_traffic_intensity(key, data)
             self.extract_physical_features(key, data)
             self.extract_biodiversity(key, data)
             self.extract_fisheries(key, data)
             self.extract_bathymetry(key, data)
-            print(data)
 
-
+        self.export_data_to_csv(data)
     def extract_traffic_intensity(self, key, data):
 
         entity_name = self.__data[key]['environment']['regional_parameters'][0]['internal_name']
@@ -110,7 +156,7 @@ class DataTransformer:
         else:
 
             print(' Loading data from API ')
-            res = self.egeos_authentication_api()
+            res = egeos_authentication_api()
             access_token = res['session_id']
             key = res['key']
             headers = {"language": "eng", "access_token": access_token}
@@ -131,49 +177,12 @@ class DataTransformer:
 
         return finding_objects
 
-    def egeos_authentication_api(self):
+    def export_data_to_csv(self, data):
 
-        base_url = app.config['EGEOS']['base_url']
-        r_login_request = requests.post(base_url + '/auth/login_request',
-                                        data={'username': app.config['EGEOS']['user_name']})
-        data = r_login_request.json()
-        if "code" not in data or data["code"] != 401005:
-            print(r_login_request.json())
-            challenge = data['challenge']
-            login_id = data['login_id']
-            salt = data['salt']
-            password = app.config['EGEOS']['password']
+        file_name = "amucad_dataset.csv"
+        if os.path.exists(file_name):
+            os.remove(file_name)
 
-            salt = base64.b64decode(salt)
-            challenge = base64.b64decode(challenge)
-            concated = salt + password.encode('utf-8')
-            currentHash = concated
-            i = 0
-            while i < 100000:
-                hash_object = hashlib.sha256()
-                hash_object.update(currentHash)
-                currentHash = hash_object.digest()
-                i += 1
-            result = salt + currentHash
-            digest1 = "digest1:" + str(base64.b64encode(result), 'utf-8')
-
-            currentHash = salt + challenge + currentHash
-            i = 0
-            while i < 5:
-                hash_object2 = hashlib.sha256()
-                hash_object2.update(currentHash)
-                currentHash = hash_object2.digest()
-                i += 1
-
-            challenge_response = str(base64.b64encode(currentHash), 'utf-8')
-            headers = {"Accept-Language": "en-US,en;q=0.9,ur;q=0.8", "language": "eng"}
-            challenge = str(base64.b64encode(challenge), 'utf-8')
-            r_login = requests.post('https://www.amucad.org/auth/login',
-                                    data={'login_id': login_id, 'challenge': challenge,
-                                          'challenge_response': challenge_response}, headers=headers)
-            return r_login.json()
-        else:
-            return data
-
-    def export_data_to_csv(self):
-        return 'awais'
+        data_frame = pd.DataFrame.from_dict(data, orient='index')
+        data_frame.to_csv(r'amucad_dataset.csv', index=False)
+        
